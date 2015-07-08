d62095fb33b46feceaa073f7003091b736c59c83
Justin Tadlock <justin@justintadlock.com>
2015-07-08 17:32:52 -0500

    Version bump to 1.0.0.

--- custom-header-extended.php
+++ custom-header-extended.php
@@ -3,7 +3,7 @@
  * Plugin Name: Custom Header Extended
  * Plugin URI:  http://themehybrid.com/plugins/custom-header-extended
  * Description: Allows users to create <a href="http://codex.wordpress.org/Custom_Headers">custom headers</a> for individual posts, which are displayed on the single post page.  It works alongside any theme that supports the WordPress <code>custom-header</code> feature.
- * Version:     0.1.1
+ * Version:     1.0.0
  * Author:      Justin Tadlock
  * Author URI:  http://justintadlock.com
  * Text Domain: custom-header-extended
@@ -24,7 +24,7 @@
  * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * @package   CustomHeaderExtended
- * @version   0.1.1
+ * @version   1.0.0
  * @since     0.1.0
  * @author    Justin Tadlock <justintadlock@gmail.com>
  * @copyright Copyright (c) 2013 - 2015, Justin Tadlock
